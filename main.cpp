1d6f3b1d5c59f50a354b4a49c3af52d9df2c9e9c
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-08-03 20:23:32 -0700

    up

--- main.cpp
+++ main.cpp
@@ -12,17 +12,7 @@ _NSTD_UNSIGNED
 
 int main()
 {
-	nstd::LongInt li(rand());
-	nstd::LongInt li2(li);
-	uint ui(rand());
-
-	_STD cout << li << '\n' << nstd::LongInt(ui) << "\n\n";
-
-	_STD cout << "Li + Ui: " << static_cast<uint>(li + ui) << " v " << (ui + li) << " (" << static_cast<uint>(li + ui) - (ui + li) << ")\n";
-	_STD cout << "Li + Ui - Ui: " << static_cast<uint>(li + ui - ui) << " v " << (ui + li - ui) << " (" << static_cast<uint>(li + ui - ui) - (ui + li - ui) << ")\n";
-	_STD cout << "Li * Ui: " << static_cast<uint>(li * ui) << " v " << (ui * li) << " (" << static_cast<uint>(li * ui) - (ui * li) << ")\n";
-	_STD cout << "Li / Ui: " << static_cast<uint>(li / ui) << " v " << (static_cast<uint>(li) / ui) << " (" << (static_cast<uint>(li / ui) - static_cast<uint>(li) / ui) << ")\n";
-	_STD cout << "Li % Ui: " << static_cast<uint>(li % ui) << " v " << (static_cast<uint>(li) % ui) << " (" << (static_cast<uint>(li % ui) - static_cast<uint>(li) % ui) << ")\n";
+	nstd::LongInt(nstd::LongInt(1))
 
 	std::cout << '\n';
 	std::system("pause");
