102aee7af883e96691e1fb0562d8dc93fb8b7e2d
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-08-16 15:56:06 -0700

    åp

--- Machine_Code_Emulator.h
+++ Machine_Code_Emulator.h
@@ -2,34 +2,108 @@
 #ifndef _NSTD_MACHINE_CODE_EMULATOR_
 
 #include "Defines.h"
-#include <string>
+#include <iostream>
+#include <vector>
 
 _NSTD_BEGIN
 
 class emulator {
 	struct Token {
+		using storage_t = uchar;
 		static enum Type {
-			DATA = 0,
-			COMMAND,
-			UNKNOWN,
+			TERMINATE = 0,
+
+			ADD,
+			SUB,
+			STORE,
+
+			LD = 5,
+			JMP,
+			JMP_0,
+			JMP_POS,
+
+			IN = 901,
+			OUT,
+
+			UNKNOWN = 999,
 		};
-		Token(Type t, uint data) : type_(t), data_(data) {}
+		Token(Type t, storage_t data) : type_(t), data_(data) {}
 
 		Type type_;
-		uint data_;
+		storage_t data_;
 	};
 
 public:
-	emulator() {}
-	emulator(const _STD string& command) {
-		construct(command);
+	static _STD vector<Token> parse(_STD vector<uint> input) {
+		_STD vector<Token> out;
+		_NSTD_FOR_I(input.size()) {
+			Token::Type t = Token::Type::UNKNOWN;
+			Token::storage_t data = input[_I] % 100;
+			switch (input[_I] / 100) {
+			case 0: if (input[_I] == 0) 
+					t = Token::Type::TERMINATE;	break;
+			case 1: t = Token::Type::ADD;		break;
+			case 2: t = Token::Type::SUB;		break;
+			case 3: t = Token::Type::STORE;		break;
+			case 5: t = Token::Type::LD;		break;
+			case 6: t = Token::Type::JMP;		break;
+			case 7: t = Token::Type::JMP_0;		break;
+			case 8: t = Token::Type::JMP_POS;	break;
+			case 9: if (input[_I] == 901) t = Token::Type::IN; else if (input[_I] == 902) t = Token::Type::OUT; break;
+			}
+			out.push_back(Token(t, data));
+		}
+		return out;
 	}
 
-	void construct(const _STD string& command) {
-		_NSTD_FOR_I(command.length()) {
+	static void run(_STD vector<uint> input) {
+		run(parse(input));
+	}
 
+	static void run(_STD vector<Token> tokens) {
+		tokens.resize(100, Token(Token::Type::TERMINATE, 0));
+		Token::storage_t reg(0);
+		
+		_NSTD_FOR_I(tokens.size()) {
+			switch (tokens[_I].type_) {
+			case Token::Type::TERMINATE: return;
+			case Token::Type::ADD:
+				reg += tokens[tokens[_I].data_].data_;
+				break;
+			case Token::Type::SUB:
+				reg -= tokens[tokens[_I].data_].data_;
+				break;
+			case Token::Type::STORE:
+				tokens[tokens[_I].data_].data_ = reg;
+				break;
+			case Token::Type::LD:
+				reg = tokens[_I].data_;
+				break;
+			case Token::Type::JMP:
+				_I = tokens[_I].data_;
+				break;
+			case Token::Type::JMP_0:
+				if (!reg)
+					_I = tokens[_I].data_;
+				break;
+			case Token::Type::JMP_POS:
+				if (reg > 0)
+					_I = tokens[_I].data_;
+				break;
+			case Token::Type::IN:
+				_STD cout << "\n901 called, please input data: ";
+				uint cache;
+				_STD cin >> cache;
+				reg = static_cast<Token::storage_t>(cache);
+				break;
+			case Token::Type::OUT:
+				_STD cout << "\n902 output: " << static_cast<uint>(reg);
+				break;
+			}
 		}
 	}
+
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -6,51 +6,16 @@
 //#define _NSTD_LONGINT_DEBUGGING_
 
 #include "LongInt.h"
+#include "Machine_Code_Emulator.h"
 //#include "Deque.h"
 //#include "Timer.h"
 
 _NSTD_UNSIGNED
 
-struct dictionary {
-	static bool is_word(const char* w) {
-		return true;
-	}
-};
-
-constexpr char** int_to_char{ {'a', 'b', 'c'}, {'e', 'f', 'g'} };
-
-
-
-_STD vector<_STD vector<char>> word_arr(const _STD vector <char> prev_list, const _STD vector<int> ints, int index) {
-	_STD vector<char> temp(prev_list);
-	_STD vector<_STD vector<char>> out;
-	
-	if (index == ints.size()) {
-		_NSTD_FOR_I((ints[index] == 7 || ints[index] == 9 ? 4 : 3)) {
-			temp.push_back(int_to_char[ints[index]][_I]);
-			if (dictionary::is_word(temp.data()))
-				out.push_back(temp);
-			temp.pop_back();
-		}
-	} else {
-		_NSTD_FOR_I((ints[index] == 7 || ints[index] == 9 ? 4 : 3)) {
-			_STD vector<_STD vector<char>> out_rec(word_arr(temp.push_back(int_to_char[ints[index]][_I]), ints, index + 1));
-			if (out_rec.size()) {
-				out.insert(out.begin(), out_rec.begin(), out_rec.end());
-			}
-			temp.pop_back();
-		}
-	}
-	return out;
-}
-
-
-
-
 
 int main()
 {
-	const char* in;
+	nstd::emulator::run(_STD vector<uint> {901, 705, 111, 311, 600, 511, 902});
 	
 
 	std::cout << '\n';
