4c5eb9a4cc03bfde4062390de020a0eec831de37
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-12-27 17:40:27 -0800

    main

--- main.cpp
+++ main.cpp
@@ -6,30 +6,23 @@
 #define _NSTD_LONGINT_TESTER_
 
 #include "LongInt.h"
-#include "Machine_Code_Emulator.h"
+//#include "Machine_Code_Emulator.h"
 //#include "Deque.h"
 //#include "Timer.h"
 
 _NSTD_UNSIGNED
 
-#include <thread>
-#include <windows.h>
 
 int main()
 {
-//  nstd::LongInt li(578);
-//  _STD cout << static_cast<uint>(li -= 100) << '\n';
-//  _STD cout << static_cast<uint>(li % 10); //<< " " << (static_cast<uint>(li) % 10);
 
+	_NSTD _LONGINT_TESTER lit;
+	lit.test1();
+	lit.test2();
+	lit.test3();
     
 
-/*  vector<string> s({"word", "good", "best", "good"});
-    auto v = Solution::findSubstring("wordgoodgoodgoodbestword", s);
-    if (!v.size())
-        cout << "\nn";
-    for (int i : v)
-        cout << i << '\t';
-*/
+
 	std::cout << '\n';
 	std::system("pause");
 }
