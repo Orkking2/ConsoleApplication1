42471d8c2c260c91d5c4cf95a10a639743a233b4
Orkking2 <nicolas.bird.voneyben@gmail.com>
2023-01-02 02:54:43 -0800

    typechecking

--- BinTree.h
+++ BinTree.h
@@ -16,12 +16,21 @@ struct _Default_comparitor_t {
 };
 
 template <typename _Val, typename _Key, typename _Comparitor>
-struct _Default_node_t {
-	// Will make a copy of Val input
-	_Default_node_t(const _Val& v, const _Key& k, _Default_node_t* l, _Default_node_t* r) : _val(v), _key(k), _left(l), _right(r) {}
+class _Default_node_t {
+	using _Mykey_t		= _Key;
+	using _Myval_t		= _Val;
+	using _Mycompare_t	= _Comparitor;
 
-	const _Val& GetVal(const _Key& key) const {
-		switch (_Comparitor::compare(key, _key)) {
+	// Maybe relevant maybe not
+	static_assert(_STD is_same_v<decltype(&_Mycompare_t::compare), _STD strong_ordering(_Mycompare_t::*)(_NSTD add_cr_t<_Mykey_t>, _NSTD add_cr_t<_Mykey_t>)>,
+		"T must have static method _STD strong_ordering compare(const _Key&, const _Key&)");
+
+public:
+	// Will make a copy of Val input
+	_Default_node_t(const _Myval_t& v, const _Mykey_t& k, _Default_node_t* l, _Default_node_t* r) : _val(v), _key(k), _left(l), _right(r) {}
+
+	_NODISCARD const _Myval_t& GetVal(const _Key& key) const {
+		switch (_Mycompare_t::compare(key, _key)) {
 		case _STD strong_ordering::greater:
 			_NSTD_ASSERT(_right != NULL && _right != nullptr, "_right is null");
 			return _right->GetVal(key);
@@ -33,36 +42,44 @@ struct _Default_node_t {
 		}
 	}
 
-	_Key _key;
-	_Val _val;
+private:
+	_Mykey_t _key;
+	_Myval_t _val;
 	_Default_node_t* _left, * _right;
 };
 
 template <typename _Key, typename _Val = void*, typename _Comparitor = _Default_comparitor_t<_Key>, typename _Node = _Default_node_t<_Key, _Val, _Comparitor>>
 class BinTree {
-	using _Mykey_t		= _Key;
-	using _Myval_t		= _Val;
-	using _Mynode_t		= _Node;
-	using _Mycompare_t	= _Comparitor;
+	using _Mykey_t		= _NSTD remove_crv_t<_Key>;
+	using _Myval_t		= _NSTD remove_crv_t<_Val>;
+	using _Mycompare_t	= _NSTD rebind_t<_Comparitor, _Mykey_t>;
+	using _Mynode_t		= _NSTD rebind_t<_Node, _Mykey_t, _Myval_t, _Mycompare_t>;
 
 	static_assert(_STD is_same_v<decltype(&_Mycompare_t::compare), _STD strong_ordering (_Mycompare_t::*)(_NSTD add_cr_t<_Mykey_t>, _NSTD add_cr_t<_Mykey_t>)>,
-		"T must have static method: _STD strong_ordering compare(const _Key&, const _Key&)");
+		"T must have static method _STD strong_ordering compare(const _Key&, const _Key&)");
 
-	static_assert( _STD is_construcible_v<_Mynode_t, _NSTD add_cr_t<_Myval_t>, _NSTD add_cr_t<_Mykey_t>, _STD add_pointer_t<_Mynode_t>, _STD add_pointer_t<_Mynode_t>>,
+	static_assert( _STD is_constructible_v<_Mynode_t, _NSTD add_cr_t<_Myval_t>, _NSTD add_cr_t<_Mykey_t>, _STD add_pointer_t<_Mynode_t>, _STD add_pointer_t<_Mynode_t>>,
 		"_Node must have constructor _Node(const _Val&, const _Key&, _Node_t*, _Node_t*)");
 
-	static_assert(_STD is_save_v<_NSTD add_cr_t<_Val>, decltype(_STD declval<_STD add_const_t<_Mynode_t>>().compare(_STD declval<_NSTD add_cr_t<_Mykey_t>>()))>,
-		"_Node must have nonstatic method: const _Val& GetVal(const _Key&) const;");
+	static_assert(_STD is_same_v<_NSTD add_cr_t<_Val>, decltype(_STD declval<_STD add_const_t<_Mynode_t>>().compare(_STD declval<_NSTD add_cr_t<_Mykey_t>>()))>,
+		"_Node must have nonstatic method const _Val& GetVal(const _Key&) const;");
 
 
 
 public:
 	using key_type			= _Mykey_t;
-	using node_type			= _Mynode_t;
 	using value_type		= _Myval_t;
 	using comparitor_type	= _Mycompare_t;
+	using node_type			= _Mynode_t;
 
 
+
+	_NODISCARD const _Myval_t& GetVal(const _Mykey_t& key) {
+		return _Head_node.GetVal(key);
+	}
+
+private:
+	_Mynode_t* _Head_node;
 };
 
 
