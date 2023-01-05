172d670d7ba00e856a027afcdad6fd8ede9f21c7
Orkking2 <nicolas.bird.voneyben@gmail.com>
2023-01-04 16:28:05 -0800

    _HAS_CONCEPT added

--- Defines.h
+++ Defines.h
@@ -66,5 +66,9 @@ _NSTD_END
 #define _LOW_BIT(type)  _GET_BIT(type, 0)
 #define _ALL_BIT(type)  (_HIGH_BIT(type) - 1 + _HIGH_BIT(type))
 
+// concept _Has_##name##_v
+#define _HAS_CONCEPT(name, ...) template <typename, typename = _STD void_t<>> struct _Has_##name : _STD false_type {};										\
+						template <typename T> struct _Has_##name <T, _STD void_t<decltype(_STD declval<T>().##name##( __VA_ARGS__ ))>> : _STD true_type {};	\
+						template <typename T> concept _Has_##name##_v = _Has_length<T>::value;
 
 #endif // ifndef _NSTD_DEFINES_
