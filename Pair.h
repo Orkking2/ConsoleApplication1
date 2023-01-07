d9016dd254fcad241cde4433464e36533b711522
Orkking2 <nicolas.bird.voneyben@gmail.com>
2023-01-07 02:53:31 -0800

    removed unnecessary methods

--- Pair.h
+++ Pair.h
@@ -7,22 +7,15 @@
 _NSTD_BEGIN
 
 template <typename _Ty1, typename _Ty2>
-class pair {
-
-public:
+struct pair {
 	using first_type  = _Ty1;
 	using second_type = _Ty2;
 
-
-public:
 	pair(_Ty1 first, _Ty2 second) : first(first),       second(second) {}
 	pair(const pair& other)       : first(other.first), second(other.second) {}
 #ifdef _UTILITY_
 	pair(const _STD pair<_Ty1, _Ty2>& other) : first(other.first), second(other.second) {}
 #endif
-
-	_Ty1 get_first() { return first; }
-	_Ty2 get_second() { return second; }
 
 	_Ty1 first;
 	_Ty2 second;
